7fd30fdbb608c8256edc4e46ea217df085052fe5
Nihanth2025 <sk1441@gmail.com>
2025-04-09 16:18:58 +0530

    Add files via upload

---
+++ HW.py
@@ -0,0 +1,10 @@
+season = input("Enter the season: ")
+
+if season == "summer":
+    print("Wear cotton clothes")
+
+elif season == "Rainy":
+    print("Wear raincoat")
+
+else:
+    print("Wear woolen clothes")
